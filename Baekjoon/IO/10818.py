97dd7fdc7c51aab5f2e96782aac2ecdca687a0d0
sunwookim <sunkim09@g.skku.edu>
2023-06-15 16:09:45 +0900

    [I/O] 10818 최소, 최대

---
+++ Baekjoon/IO/10818.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+num_list = list(map(int, input().split()))
+max = num_list[0] 
+min = num_list[0]
+
+for i in num_list:
+    if i > max: max = i
+    if i < min: min = i
+
+print("{} {}".format(min, max))
